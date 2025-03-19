859fe6b4333d34a8212042b628a68891f46de71a
MasterHacker10 <willikh36@gmail.com>
2025-03-19 17:21:02 -0400

    done

--- RN - Robomania/Assets/Scripts/Crusher1.cs
+++ RN - Robomania/Assets/Scripts/Crusher1.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crusher1 : MonoBehaviour
-    
-    
-    
-    {
+public class Crusher1 : MonoBehaviour {
+
+    public float speed = 5f;
+    void Start() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+    }
 }
+
+//(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬))
---
+++ RN - Robomania/Assets/Scripts/Crusher2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher2 : MonoBehaviour {
+
+    public float speed = 5f;
+    void Start() 
+        
+        {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+    }
+}
