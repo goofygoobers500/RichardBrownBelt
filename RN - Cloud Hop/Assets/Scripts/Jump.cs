c5187d404c4ca90893ecc00167c46710347a4403
GoodyGoobers500 <TheArsenalDevil@gmail.com>
2025-04-02 17:40:38 -0400

    l(●ˇ∀ˇ●)(●ˇ∀ˇ●)(●ˇ∀ˇ●)(●ˇ∀ˇ●)(●ˇ∀ˇ●)(┬┬﹏┬┬)(┬┬﹏┬┬)=D=D(●'◡'●)(●'◡'●)^_____^ヾ(≧▽≦*)o╰(*°▽°*)╯╰(*°▽°*)╯o((>ω< ))o(●'◡'●)☆*: .｡. o(≧▽≦)o .｡.:*☆(❁´◡`❁)(●ˇ∀ˇ●)(●ˇ∀ˇ●)☆*: .｡. o(≧▽≦)o .｡.:*☆☆*: .｡. o(≧▽≦)o .｡.:*☆=D╰(*°▽°*)╯=)(●'◡'●)(✿◡‿◡)^_____^

--- RN - Cloud Hop/Assets/Scripts/Jump.cs
+++ RN - Cloud Hop/Assets/Scripts/Jump.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour
-{
-    private Rigidbody2D rb;
+public class JUmp : MonoBehaviour { 
+
+    private Rigidbody2D rb;                                                                                                                                                                                                                                                                                                                         
     private float jumpForce = 15;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        Debug.Log("SIMGA BOYOIOIOII");   
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0) 
-    
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+         if (Input.GetButtonDown("Jump") && rb.velocity.y ==0)
+            {
+
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
