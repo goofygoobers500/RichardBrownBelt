7f463301f91745b2c3886096d832d63ab6f7b560
godzilaer <>
2025-04-09 16:53:06 -0400

    goob sigma simga gimsgismigmasimdgiasdgjnasljdgnlkasjgdnlkjasdngk

---
+++ RN - Cloud Hop/Assets/PY/Scripts/Jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float jumpForce = 15;
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("SIMGA BOYOIOIOII");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0) {
+
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
