1ac107a702b29ea5853d5b7474ee030fb171d662
godzilaer <>
2025-04-09 17:56:58 -0400

    aijsdf

---
+++ JungleEscape/Assets/Scripts/Jump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float jumpForce = 5.7f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump")){
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
