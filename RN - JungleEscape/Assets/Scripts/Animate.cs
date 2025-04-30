f856b938d94f90c9252dfab3361a9b2293949e16
DaKillerDonut <dylantaocallahan@gmail.com>
2025-04-30 18:01:27 -0400

    oiuytrfdes

---
+++ RN - JungleEscape/Assets/Scripts/Animate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    Animator animator;
+    Jump jump;
+    Movement movement;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        jump = GetComponent<Jump>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("grounded", jump.isGrounded);
+        animator.SetFloat("speed", movement.speed);
+    }
+}
